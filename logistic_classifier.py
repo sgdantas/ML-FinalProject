844452c0fab2ebe3befa8fdcb515d4737e8d38c4
gaddamanil16 <gaddamanil16@gmail.com>
2018-12-05 00:11:21 -0500

    for cora dataset

--- logistic_classifier.py
+++ logistic_classifier.py
@@ -8,8 +8,10 @@ Created on Tue Nov 20 14:35:17 2018
 from utils import load_data
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+from sklearn.model_selection import KFold
+kf = KFold(5, True, 1)
 
-dataset_str = "cora"  # or citeseer, pubmed. All citation ests used in Kipf paper
+dataset_str = "citeseer"  # or citeseer, pubmed. All citation ests used in Kipf paper
 
 adj, features, labels, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(dataset_str)
 
@@ -35,14 +37,21 @@ CV_rfc.fit(x_train, y_train)
 
 CV_rfc.best_params_
 #%%
-clf_rf = RandomForestClassifier(n_estimators=500,criterion='gini', max_depth=400,max_features='log2',random_state=42).fit(x_train, y_train)
-rf_probs = clf_rf.predict_proba(x_test)
-clf_rf.score(x_test, y_test)
-#%%
-from sklearn.metrics import roc_auc_score
+accuracy = []
+for train_index, test_index in kf.split(features):
+        X_train, X_test = features[train_index], features[test_index]
+        y_train, y_test = labels_class[train_index], labels_class[test_index]
+        clf_rf = RandomForestClassifier(n_estimators=500,criterion='gini', max_depth=400,max_features='log2',random_state=42).fit(X_train, y_train)
+        #rf_probs = clf_rf.predict_proba(X_test)
+        accuracy.append(clf_rf.score(X_test, y_test))
 
-# Calculate roc auc
-roc_value = roc_auc_score(y_test, rf_probs)
 #%%
-clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial').fit(x_train, y_train)
-clf.score(x_test, y_test)
+accuracy_logistic = []
+for train_index, test_index in kf.split(features):
+        X_train, X_test = features[train_index], features[test_index]
+        y_train, y_test = labels_class[train_index], labels_class[test_index]
+        clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial').fit(X_train, y_train)
+        accuracy_logistic.append(clf.score(X_test, y_test))
+#%%
+import statistics
+print(statistics.stdev(accuracy_logistic))
